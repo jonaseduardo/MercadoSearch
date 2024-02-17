c40601b33846f2ae5fded9a3365280967811ca30
Jonathan García <jonat10@gmail.com>
2024-02-17 12:48:46 -0300

    Se implementan delegates

--- MercadoSearchUIKit/ViewController.swift
+++ MercadoSearchUIKit/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var searchController: UISearchController!
 
     override func viewDidLoad() {
@@ -16,8 +15,21 @@ class ViewController: UIViewController {
         
         searchController = UISearchController(searchResultsController: nil)
         searchController.searchBar.placeholder = "Search Products"
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
         
         navigationItem.searchController = searchController
     }
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+    }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        searchController.showsSearchResultsController = true
+    }
+}
