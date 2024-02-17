5946684873dfe9655694c190c17ba420034755af
Jonathan García <jonat10@gmail.com>
2024-02-17 12:19:18 -0300

    Initial Commit

---
+++ MercadoSearchUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MercadoSearchUIKit
+//
+//  Created by Jonathan García on 17/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
